412cf43d59ab0b9448f88ca8f948531676ff8f9e
vhhv <henryvuoj@gmail.com>
2025-04-25 14:56:02 +0700

    fix: finish create heapify

--- class7_heap/monk_and_multiplication.cpp
+++ class7_heap/monk_and_multiplication.cpp
@@ -7,6 +7,87 @@
 
 using namespace std;
 
-int main() {
+//HEAP IMPLEMENTATION
+void _swap(vector<int> &nums, int i, int j) {
+    int tmp = nums[i];
+    nums[i] = nums[j];
+    nums[j] = tmp;
+}
+void _heapify(vector<int> &nums, bool isMinHeap, int current){
+    int nums_size = nums.size();
+    int left_child = current * 2 + 1;
+    int right_child = current * 2 + 2;
+    if (isMinHeap) {
+        if (left_child < nums_size && right_child < nums_size) {
+            int swap_value = std::min(nums[left_child], nums[right_child]);
+            if (swap_value < nums[current]) {
+                if (swap_value == nums[left_child]) {
+                    _swap(nums, left_child, current);
+                    _heapify(nums, isMinHeap, left_child);
+                } else {
+                    _swap(nums, right_child, current);
+                    _heapify(nums, isMinHeap, right_child);
+                }
+            }
+        } else {
+            if (left_child < nums_size && nums[left_child] < nums[current]) {
+                _swap(nums, left_child, current);
+                _heapify(nums, isMinHeap, left_child);
+            } else if (right_child < nums_size && nums[right_child] < nums[current]) {
+                _swap(nums, right_child, current);
+                _heapify(nums, isMinHeap, right_child);
+            }
+        }
+    } else {
+        if (left_child < nums.size() && right_child < nums_size) {
+            int swap_value = std::max(nums[left_child], nums[right_child]);
+            if (swap_value > nums[current]) {
+                if (swap_value == nums[left_child]) {
+                    _swap(nums, left_child, current);
+                    _heapify(nums, isMinHeap, left_child);
+                } else {
+                    _swap(nums, right_child, current);
+                    _heapify(nums, isMinHeap, right_child);
+                }
+            }
+        } else {
+            if (left_child < nums_size && nums[left_child] > nums[current]) {
+                _swap(nums, left_child, current);
+                _heapify(nums, isMinHeap, left_child);
+            } else if (right_child < nums_size && nums[right_child] > nums[current]) {
+                _swap(nums, right_child, current);
+                _heapify(nums, isMinHeap, right_child);
+            }
+        }
+    }
+}
+void heapify(vector<int> &nums, bool isMinHeap) {
+    //Start from parent which most close to child parent to the right
+    //Start checking the parent with all child node, if child node > or < then replace the parent
+    //Continue the heapify with lower child
+    int current = nums.size()/2 - 1;
+    while(current >= 0) {
+      _heapify(nums, isMinHeap, current);
+      current--;
+    }
+}
+void test_heapify() {
+    vector<int> nums = {2,4,7,10,8,3,5,1,9,6};
 
+    heapify(nums, true);
+    for(int i = 0; i < nums.size(); i++) {
+        cout << nums[i] << " ";
+    }
+    printf("Done Min Heapify\n");
+
+    heapify(nums, false);
+    for(int i = 0; i < nums.size(); i++) {
+        cout << nums[i] << " ";
+    }
+    printf("Done Max Heapify\n");
+}
+//HEAP IMPLEMENTATION
+
+int main() {
+    test_heapify();
 }
