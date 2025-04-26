3864c0776c668f565d33802685f78131817f319a
vhhv <henryvuoj@gmail.com>
2025-04-26 11:35:11 +0700

    class 7: monk and multiplication

--- class7_heap/monk_and_multiplication.cpp
+++ class7_heap/monk_and_multiplication.cpp
@@ -3,17 +3,17 @@
 //
 
 #include <iostream>
-#include <algorithm>
+#include <vector>
 
 using namespace std;
 
 //HEAP IMPLEMENTATION
-void _swap(vector<int> &nums, int i, int j) {
+void _swap(vector<uint64_t> &nums, int i, int j) {
     int tmp = nums[i];
     nums[i] = nums[j];
     nums[j] = tmp;
 }
-void _heapify(vector<int> &nums, bool isMinHeap, int current){
+void _heapify(vector<uint64_t> &nums, bool isMinHeap, int current){
     int nums_size = nums.size();
     int left_child = current * 2 + 1;
     int right_child = current * 2 + 2;
@@ -61,7 +61,7 @@ void _heapify(vector<int> &nums, bool isMinHeap, int current){
         }
     }
 }
-void heapify(vector<int> &nums, bool isMinHeap) {
+void heapify(vector<uint64_t> &nums, bool isMinHeap) {
     //Start from parent which most close to child parent to the right
     //Start checking the parent with all child node, if child node > or < then replace the parent
     //Continue the heapify with lower child
@@ -71,7 +71,7 @@ void heapify(vector<int> &nums, bool isMinHeap) {
       current--;
     }
 }
-void insert_heap(vector<int> &nums, bool isMinHeap, int value) {
+void insert_heap(vector<uint64_t> &nums, bool isMinHeap, int value) {
     nums.push_back(value);
     int current = nums.size() - 1;
     while (current > 0) {
@@ -88,7 +88,7 @@ void insert_heap(vector<int> &nums, bool isMinHeap, int value) {
         current = parent_index;
     }
 }
-void remove_heap(vector<int> &nums, bool isMinHeap) {
+void remove_heap(vector<uint64_t> &nums, bool isMinHeap) {
     if (nums.size() > 0) {
         _swap(nums, 0, nums.size() - 1);
         nums.pop_back();
@@ -96,7 +96,7 @@ void remove_heap(vector<int> &nums, bool isMinHeap) {
     }
 }
 void test_heap() {
-    vector<int> nums = {2,4,7,10,8,3,5,1,9,6};
+    vector<uint64_t> nums = {2,4,7,10,8,3,5,1,9,6};
 
     heapify(nums, true);
     // insert_heap(nums, true, 0);
@@ -107,7 +107,7 @@ void test_heap() {
     }
     printf("Done Min Heapify\n");
 
-    vector<int> nums2 = {2,4,7,10,8,3,5,1,9,6};
+    vector<uint64_t> nums2 = {2,4,7,10,8,3,5,1,9,6};
     heapify(nums2, false);
     for(int i = 0; i < nums2.size(); i++) {
         cout << nums2[i] << " ";
@@ -116,6 +116,53 @@ void test_heap() {
 }
 //HEAP IMPLEMENTATION
 
+void monk_and_multiplication(vector<uint64_t> nums) {
+    bool isMinHeap = false;
+    vector<uint64_t> heap = {};
+    for (uint64_t i = 0; i < nums.size(); i++) {
+        insert_heap(heap, isMinHeap, nums[i]);
+        if (heap.size() > 2) {
+            vector<uint64_t> temp;
+            uint64_t total = 1;
+            for (int j = 0; j < 3; j++) {
+                total *= heap[0];
+                temp.push_back(heap[0]);
+                remove_heap(heap, isMinHeap);
+            }
+            printf("%llu\n", total);
+            for (int j = 0; j < 3; j++) {
+                insert_heap(heap, isMinHeap, temp[j]);
+            }
+        } else {
+            printf("-1\n");
+        }
+    }
+}
+
+int test1() {
+    int n = 5;
+    vector<uint64_t> nums = {1,2,3,4,5};
+    monk_and_multiplication(nums);
+}
+
+int test2() {
+    int n = 5;
+    vector<uint64_t> nums = {474709,855829,759911,977393,157547,455037,807388};
+    monk_and_multiplication(nums);
+}
+
 int main() {
-    test_heap();
+    // test2();
+
+    uint64_t n;
+    cin >> n; // read how many numbers you want to input
+    vector<uint64_t> nums;
+
+    for (int i = 0; i < n; i++) {
+        uint64_t num;
+        cin >> num;
+        nums.push_back(num);
+    }
+
+    monk_and_multiplication(nums);
 }
