ae2d2bb67c7baec1fa22ef376ba33eb9114befcf
vhhv <henryvuoj@gmail.com>
2025-05-07 14:12:38 +0700

    fix: toposort

--- orange/class1_toposort/topo_sort.cpp
+++ orange/class1_toposort/topo_sort.cpp
@@ -165,10 +165,81 @@ void test_toposort_kahn() {
 }
 //TOPOSORT KAHN
 
+//TOPOSORT KAHN
+//Has queue to put all vertice_weight = 0 into
+vector<int> toposort_kahn_pq(vector<vector<int>> &graph) {
+    int n = graph.size();
+    vector<int> result;
+    vector<int> vertice_weight(n, 0);
+    priority_queue<int, vector<int>, greater<int>> pq;
+    for (int i = 0; i < n; ++i) {
+        for (int j = 0; j < graph[i].size(); ++j) {
+            vertice_weight[graph[i][j]]++;
+        }
+    }
+    for (int i = 0; i < n; ++i) {
+        if (vertice_weight[i] == 0) {
+            pq.push(i);
+        }
+    }
+    while (!pq.empty()) {
+        int node = pq.top(); pq.pop();
+        result.push_back(node);
+        for (int v : graph[node]) {
+            vertice_weight[v]--;
+            if (vertice_weight[v] == 0) {
+                pq.push(v);
+            }
+        }
+    }
+    return result;
+}
+void test_toposort_kahn_pq() {
+    vector<vector<int>> graph = {
+        {1},
+        {2,5,6},
+        {},
+        {5, 7},
+        {1, 7},
+        {},
+        {},
+        {6}
+    };
+    //Result is 2,5,6,1,0,7,3,4
+    vector<int> result = toposort_kahn_pq(graph);
+    cout << "result" << endl;
+}
+//TOPOSORT KAHN
+
+/* Loop 7,9,6,12
+14 20
+7 13
+4 5
+8 2
+10 13
+7 9
+5 11
+5 2
+4 9
+14 13
+8 6
+6 12
+11 2
+10 4
+12 7
+11 3
+12 2
+9 6
+11 12
+12 3
+11 13
+ */
+
 int main() {
     // test_toposort_dfs();
     // test_toposort_dfs_with_cycle();
     // test_toposort_kahn();
+    // test_toposort_kahn_pq();
 
     int n, m;
     cin >> n >> m;
@@ -177,11 +248,12 @@ int main() {
         int u, v;
         cin >> u >> v;
         u--; v--;
-        graph[v].push_back(u);
+        graph[u].push_back(v);
     }
-    vector<int> result = toposort_dfs_with_cycle(graph);
+    // vector<int> result = toposort_dfs_with_cycle(graph);
     // vector<int> result = toposort_kahn(graph);
-    if (result.empty()) {
+    vector<int> result = toposort_kahn_pq(graph);
+    if (result.size() != n) {
         cout << "Sandro fails.";
     } else {
         for (int res : result) {
