4bf5f0c14ca4fb11f07a4a865f8372f3e54d6726
vhhv <henryvuoj@gmail.com>
2025-05-12 09:17:12 +0700

    fix

--- orange/class2_bitmask/samu_birthday.cpp
+++ orange/class2_bitmask/samu_birthday.cpp
@@ -3,7 +3,10 @@
 //
 
 #include <iostream>
+#include <map>
 #include <vector>
+#include <algorithm>
+
 using namespace std;
 
 /*
@@ -12,6 +15,28 @@ using namespace std;
 100001
 010001
 001001
+
+
+1
+3 3
+110
+011
+101
+
+
+1
+3 3
+110
+011
+101
+
+
+1
+4 4
+1100
+0001
+1010
+0100
 
 
  */
@@ -25,30 +50,35 @@ int main() {
         long long res = 0;
         cin.ignore();
         vector<long long> friends(N, 0);
+        map<int, int> priority_idx;
         for (int j = 0; j < N; j++) {
             string s;
             getline(cin, s);
             for (int k = 0; k < K; k++) {
                 if (s[k] == '1') {
-                    friends[j] |= (1 << (K - 1 - k));
+                    int ik = K - 1 - k;
+                    friends[j] |= (1 << ik);
+                    priority_idx[ik]++;
                 }
             }
             res |= friends[j];
         }
-        for (int k = 0; k < K; k++) {
-            long long res_kth_bit = (res >> k) & 1;
-            if (res_kth_bit == 1) {
-                long long check_kth_bit = res & (~(1<<k));
-                bool check = true;
-                for (int j = 0; j < N; j++) {
-                    if ((check_kth_bit & friends[j]) == 0) {
-                        check = false;
-                        break;
-                    }
+        vector<int> sorted_priority_idx;
+        for (pair<int, int> p : priority_idx) {
+            sorted_priority_idx.push_back(p.first);
+        }
+        sort(sorted_priority_idx.begin(), sorted_priority_idx.end());
+        for (int k : sorted_priority_idx) {
+            long long check_kth_bit = res & (~(1<<k));
+            bool check = true;
+            for (int j = 0; j < N; j++) {
+                if ((check_kth_bit & friends[j]) == 0) {
+                    check = false;
+                    break;
                 }
-                if (check) {
-                    res = check_kth_bit;
-                }
+            }
+            if (check) {
+                res = check_kth_bit;
             }
         }
         int count = 0;
