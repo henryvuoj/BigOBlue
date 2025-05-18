a52c038eb5a8d89b598cc62e66debe3be9812c6f
Henry <henry@dewtouch.com>
2025-05-18 10:00:27 +0700

    fix: max_subarr_sum

--- orange/class4_divide_and_conquer/max_subarr_sum.cpp
+++ orange/class4_divide_and_conquer/max_subarr_sum.cpp
@@ -3,17 +3,41 @@
 //
 
 #include <vector>
-#include <algorithm>
 #include <iostream>
 #include <ostream>
+#include <algorithm>
+#define INF -10000000
 
 using namespace std;
 
-int max_subarr_sum(vector<int> arr) {
+int max_mid_subarr_sum(vector<int> arr, int left, int mid, int right) {
+    int sum = 0;
+    int left_sum = INF;
+    for (int i = mid; i >= left; i--) {
+        sum += arr[i];
+        left_sum = max(left_sum, sum);
+    }
+    sum = 0;
+    int right_sum = INF;
+    for (int i = mid + 1; i <= right; i++) {
+        sum += arr[i];
+        right_sum = max(right_sum, sum);
+    }
+    return left_sum + right_sum;
+}
 
+int max_subarr_sum(vector<int> arr, int left, int right) {
+    if (left >= right) {
+        return arr[left];
+    }
+    int mid = (right + left) / 2;
+    int max_sum_left = max_subarr_sum(arr, left, mid);
+    int max_sum_right = max_subarr_sum(arr, mid + 1, right);
+    int max_sum_mid = max_mid_subarr_sum(arr, left, mid, right);
+    return max(max(max_sum_left, max_sum_right), max_sum_mid);
 }
 
 int main() {
     vector<int> arr = {-3,-8,7,-2,-3,5,-4,6,-9,2};
-    cout << max_subarr_sum(arr) << endl;
+    cout << max_subarr_sum(arr, 0, arr.size() - 1) << endl;
 }
