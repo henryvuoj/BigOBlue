85ef127ac7d1a968fe38b5837ef088581e34e8ec
Henry <henry@dewtouch.com>
2025-06-15 22:24:33 +0700

    feat: lis binary search

--- orange/class_12_dynamic_programming_lis/lis.cpp
+++ orange/class_12_dynamic_programming_lis/lis.cpp
@@ -46,9 +46,37 @@ string lis_dynamic_print_result(vector<int> list) {
     return result;
 }
 
+int binary_search(vector<int> list, vector<int> result, int target) {
+    int left = 0;
+    int right = result.size() - 1;
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (list[result[mid]] < target) {
+            left = mid + 1;
+        } else {
+            return mid;
+        }
+    }
+    return -1;
+}
+
+int lis_dynamic_binary_search(vector<int> list) {
+    vector<int> result;
+    result.push_back(0);
+    for (int i = 1; i < list.size(); i++) {
+        int pos = binary_search(list, result, list[i]);
+        if (pos == -1) {
+            result.push_back(i);
+        } else {
+            result[pos] = i;
+        }
+    }
+    return result.size();
+}
 
 int main() {
     vector<int> list = {2, 5, 12, 3, 10, 6, 8, 14, 4, 11, 7, 15};
     // cout << lis_dynamic(list) << endl;
-    cout << lis_dynamic_print_result(list) << endl;
+    // cout << lis_dynamic_print_result(list) << endl;
+    cout << lis_dynamic_binary_search(list) << endl;
 }
