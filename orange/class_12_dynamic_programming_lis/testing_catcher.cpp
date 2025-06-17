b8f650d3c0bb558fcef43abb60a6f218ab29cea1
vhhv <henryvuoj@gmail.com>
2025-06-17 15:34:24 +0700

    fix: testing the catcher

--- orange/class_12_dynamic_programming_lis/testing_catcher.cpp
+++ orange/class_12_dynamic_programming_lis/testing_catcher.cpp
@@ -21,19 +21,17 @@ int lis(vector<int> v) {
 }
 
 int lds(vector<int> v) {
-    vector<int> dp(v.size());
-    dp[0] = 1;
+    vector<int> dp(v.size(), 1);
+    int maxVal = 1;
     for (int i = 1; i < v.size(); i++) {
         for (int j = 0; j < i; j++) {
             if (v[j] >= v[i]) {
                 dp[i] = max(dp[i], dp[j] + 1);
+                maxVal = max(maxVal, dp[i]);
             }
         }
     }
-    if (dp[v.size() - 1] == 0) {
-        return -1;
-    }
-    return dp[v.size() - 1];
+    return maxVal;
 }
 
 int binary_search(vector<int> v, vector<int> result, int target) {
@@ -69,33 +67,35 @@ int main() {
     // vector<int> arr = {23, 34, 21};
     // vector<int> arr = {23, 34, 56};
     // cout << lds(arr) << endl;
-    vector<int> arr = {33, 33, 33, 34, 34, 34};
-    cout << lds_binary_search(arr) << endl;
+    // vector<int> arr = {33, 33, 33, 34, 34, 34};
+    // cout << lds_binary_search(arr) << endl;
+    // cout << lds(arr) << endl;
 
-    // int count = 1;
-    // while (true) {
-    //     vector<int> v;
-    //     int i;
-    //     cin>>i;
-    //     if (i == -1) {
-    //         break;
-    //     } else {
-    //         v.push_back(i);
-    //     }
-    //     while (cin>>i) {
-    //         if (i == -1) {
-    //             break;
-    //         } else {
-    //             v.push_back(i);
-    //         }
-    //     }
-    //     int res = lds_binary_search(vector<int>(v));
-    //     res = res == 0 ? -1 : res;
-    //     // int res = lds(vector<int>(v));
-    //     cout<<"Test #"<<count<<":"<<endl;
-    //     cout<<"  maximum possible interceptions: "<<res<<endl;
-    //     cout<<endl;
-    //     count++;
-    // }
+    int count = 1;
+    while (true) {
+        vector<int> v;
+        int i;
+        cin>>i;
+        if (i == -1) {
+            break;
+        } else {
+            v.push_back(i);
+        }
+        while (cin>>i) {
+            if (i == -1) {
+                break;
+            } else {
+                v.push_back(i);
+            }
+        }
+        // int res = lds_binary_search(vector<int>(v));
+        int res = lds(vector<int>(v));
+        // res = res == 0 ? -1 : res;
+        // int res = lds(vector<int>(v));
+        cout<<"Test #"<<count<<":"<<endl;
+        cout<<"  maximum possible interceptions: "<<res<<endl;
+        cout<<endl;
+        count++;
+    }
 
 }
