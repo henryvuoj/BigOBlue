5b289de83dbc890d509139f1833239e9523f02c3
Henry <henry@dewtouch.com>
2025-06-22 18:28:23 +0700

    fix: balo

--- orange/class_13_dynamic_programming_knapsack/balo.cpp
+++ orange/class_13_dynamic_programming_knapsack/balo.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-int knapsack(int max_weight, vector<pair<int, int>> items) {
+vector<vector<int>> knapsack_core(int max_weight, vector<pair<int, int>> items) {
     vector<vector<int>> dp(items.size()+1, vector<int>(max_weight+1, 0));
 
     for (int i = 1; i <= items.size(); i++) {
@@ -26,7 +26,35 @@ int knapsack(int max_weight, vector<pair<int, int>> items) {
             dp[i][w] = optimized_value;
         }
     }
+    return dp;
+}
+
+
+int knapsack(int max_weight, vector<pair<int, int>> items) {
+    vector<vector<int>> dp = knapsack_core(max_weight, items);
     return dp[items.size()][max_weight];
+}
+
+vector<pair<int, int>> knapsack_print_results(int max_weight, vector<pair<int, int>> items) {
+    vector<vector<int>> dp = knapsack_core(max_weight, items);
+    int i = items.size();
+    int w = max_weight;
+    vector<pair<int, int>> result_items;
+    while (dp[i][w] > 0) {
+        result_items.push_back(items[i-1]);
+        if (items[i-1].second <= w) {
+            int compensate_i = w-items[i-1].second;
+            if (dp[i-1][w] < items[i-1].first + dp[i-1][compensate_i]) {
+                w = compensate_i;
+            }
+        }
+        i--;
+    }
+    //PRINT
+    for (pair<int, int> item : result_items) {
+        cout <<"("<< item.first << " - " << item.second <<")"<< endl;
+    }
+    return result_items;
 }
 
 int main() {
@@ -37,6 +65,7 @@ int main() {
         {2,2},
         {10,4}
     };
-    int max_value = knapsack(max_weight, items);
-    cout << max_value << endl;
+    // int max_value = knapsack(max_weight, items);
+    // cout << max_value << endl;
+    knapsack_print_results(max_weight, items);
 }
