fda9f768c809d01ff6a24f213f710c30f6ff7211
Henry <henry@dewtouch.com>
2025-06-22 10:53:09 +0700

    fix

--- orange/class_12_dynamic_programming_lis/beautiful_people.cpp
+++ orange/class_12_dynamic_programming_lis/beautiful_people.cpp
@@ -127,31 +127,24 @@ vector<int> organize_optimized2(vector<Person2> people) {
             ans.push_back(i);
         } else {
             int pos = binary_search_person_b(people, ans, people[i]);
-            path[i] = ans[pos-1];
+            if (pos > 0) {
+                path[i] = ans[pos-1];
+            }
             ans[pos] = i;
         }
     }
     vector<int> res;
     int path_pos = ans.back();
-    res.push_back(path_pos);
+    res.push_back(people[path_pos].idx);
     while (path[path_pos] != -1) {
         path_pos = path[path_pos];
-        res.push_back(path_pos);
+        res.push_back(people[path_pos].idx);
     }
+    reverse(res.begin(), res.end());
     return res;
 }
 
 int main() {
-//     vector<Person> people = {
-//         {0, 3,  3},
-//         {1, 1, 1},
-//         {2,  1, 2},
-//         {3, 2, 1},
-//         {4, 2, 2}
-//     };
-// //     vector<int> invited_people = organize(people);
-//     vector<int> invited_people = organize_optimized(people);
-
     //1,4,5
     // vector<Person2> people = {
     //     {0, 1,1},
@@ -177,27 +170,7 @@ int main() {
     //     {4,  5,3},
     //     {5,  6,4}
     // };
-    /*
 
-1,1 -> 4,2
-1,1 -> 2,5 -> 3,7
-
-p [
- -1,
- 0,
- 1,
- 0,
- 1,
- 2
-]
-
-1,1
-4,2
-5,3
-6,4
-
-4,2
-     */
     // vector<int> invited_people = organize_optimized2(people);
     // cout << "DONE";
 
@@ -216,93 +189,3 @@ p [
     }
     return 0;
 }
-
-
-// #include <iostream>
-// #include <vector>
-// #include <algorithm>
-// using namespace std;
-//
-// struct Member {
-//     int s, b;
-//     int id;
-//
-//     bool operator<(const Member & member) const {
-//         return this->s == member.s ? this->b > member.b : this->s < member.s;
-//     }
-// };
-//
-// int lower_bound(const vector<Member> &a, const vector<int> &sub, int n, int x) {
-//     int left = 1;
-//     int right = n;
-//     int pos = right;
-//     while (left < right) {
-//         int mid = (left + right) >> 1;
-//         int index = sub[mid];
-//         if (a[index].b >= x) {
-//             pos = mid;
-//             right = mid;
-//         }
-//         else {
-//             left = mid + 1;
-//         }
-//     }
-//     return pos;
-// }
-//
-// int LIS(const vector<Member> & a, vector<int> & res) {
-//     vector<int> increasingSub(a.size());
-//     vector<int> path(a.size(), -1);
-//
-//     int length = 1;
-//     increasingSub[1] = 1;
-//
-//     for (int i = 2; i < a.size(); i++) {
-//         if (a[i].b > a[increasingSub[length]].b) {
-//             length++;
-//             increasingSub[length] = i;
-//             path[i] = increasingSub[length - 1];
-//         }
-//         else {
-//             int pos = lower_bound(a, increasingSub, length, a[i].b);
-//             path[i] = increasingSub[pos - 1];
-//             increasingSub[pos] = i;
-//         }
-//     }
-//
-//     int i = increasingSub[length];
-//     while (i > 0) {
-//         res.push_back(a[i].id);
-//         i = path[i];
-//     }
-//
-//     return length;
-// }
-//
-// int main() {
-//     ios::sync_with_stdio(false);
-//     int n;
-//     cin >> n;
-//     vector<Member> members(n + 1);
-//     for (int i = 1; i <= n; ++i) {
-//         cin >> members[i].s >> members[i].b;
-//         members[i].id = i;
-//     }
-//
-//     sort(members.begin() + 1, members.end());
-//     vector<int> res;
-//     cout << LIS(members, res) << endl;
-//     for (int i = res.size() - 1; i >= 0; --i) {
-//         cout << res[i] << " ";
-//     }
-// }
-
-/*
-6
-1 1
-2 5
-3 7
-4 2
-5 3
-6 4
- */
